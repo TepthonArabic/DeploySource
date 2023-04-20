e58d1957985d1b423565fc19e97e06f58534f73e
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-21 00:35:45 +0300

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -19,7 +19,7 @@ __copyright__ = f"Tepthon Copyright (C) 2021 - 2022  {__author__}"
 
 zedub.version = __version__
 zedub.tgbot.version = __version__
-LOGS = logging.getLogger("زدثــون")
+LOGS = logging.getLogger("تيبـثون")
 bot = zedub
 
 
