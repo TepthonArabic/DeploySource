e661300365a88a4d1d126039e03fd2ba5f7688c8
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-21 11:26:02 +0300

    Update startup.py

--- zthon/utils/startup.py
+++ zthon/utils/startup.py
@@ -131,7 +131,7 @@ async def mybot():
             await asyncio.sleep(1)
             await bot.send_message("@BotFather", botname)
             await asyncio.sleep(1)
-            await bot.send_message("@BotFather", "ZThon")
+            await bot.send_message("@BotFather", "Tepthon")
             await asyncio.sleep(3)
             await bot.send_message("@BotFather", "/setname")
             await asyncio.sleep(1)
