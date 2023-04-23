77542017c8a169e964a70bf5430d899b4cc8b3bc
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-23 08:46:18 +0300

    Update الفحص.py

--- zthon/plugins/الفحص.py
+++ zthon/plugins/الفحص.py
@@ -35,8 +35,8 @@ async def amireallyalive(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
-    Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✥┊"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت  تيبثـون 𝙕𝞝𝘿𝙏𝙃𝙊𝙉  يعمـل .. بنجـاح ☑️ 𓆩 **"
+    Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "❈┊"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** بـوت  تيبثـون 𝗧𝞝𝗣𝗧𝗛𝗢𝗡  يعمـل .. بنجـاح ☑️ 𓆩 **"
     ZED_IMG = gvarstatus("ALIVE_PIC")
     zed_caption = gvarstatus("ALIVE_TEMPLATE") or zed_temp
     caption = zed_caption.format(
@@ -94,7 +94,7 @@ zed_temp = """{ALIVE_TEXT}
 async def amireallyialive(event):
     "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
-    Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✥┊"
+    Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "❈┊"
     zed_caption = "** بـوت  تيبثـون 𝗧𝞝𝗣𝗧𝗛𝗢𝗡  يعمـل .. بنجـاح ☑️ 𓆩 **\n"
     zed_caption += f"**{Z_EMOJI} إصـدار التـيليثون :** `{version.__version__}\n`"
     zed_caption += f"**{Z_EMOJI} إصـدار تيبثـون :** `{zedversion}`\n"
