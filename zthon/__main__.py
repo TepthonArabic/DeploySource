3264e1a976c661babd58844280d22f3056c1c862
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-04-25 10:19:29 +0300

    Update __main__.py

--- zthon/__main__.py
+++ zthon/__main__.py
@@ -27,16 +27,16 @@ print(f"المرخصة بموجب شروط  {zthon.__license__}")
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("⌭ بـدء تنزيـل تيبثـون ⌭")
+    LOGS.info("❈ بدء تنزيل تيبثون الإصدار الثاني")
     zedub.loop.run_until_complete(setup_bot())
-    LOGS.info("⌭ بـدء تشغيـل البـوت ⌭")
+    LOGS.info("بـدء تشغيـل البـوت ❈")
 except Exception as e:
     LOGS.error(f"{e}")
     sys.exit()
 
 
 try:
-    LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
+    LOGS.info("❈ جـار تفعيـل وضـع الانـلاين ❈")
     zedub.loop.run_until_complete(mybot())
     LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
 except Exception as e:
@@ -44,7 +44,7 @@ except Exception as e:
 
 
 try:
-    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
+    LOGS.info("❈ جـاري تحميـل الملحقـات ❈")
     zedub.loop.create_task(saves())
     LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
 except Exception as e:
@@ -55,7 +55,7 @@ async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    LOGS.info(f"⌔┊تـم تنصيـب تيبثـون . . بنجـاح ✓")
+    LOGS.info(f"❈ تم تنصيب سورس تيبثون العربي الإصدار الثاني للاستفسار : @Tepthon_Help")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
