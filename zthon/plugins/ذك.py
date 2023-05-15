2a81d62dbe6aed3aafee336843be87bb524b5f93
روجر <104784913+rogerpq@users.noreply.github.com>
2023-05-15 19:06:30 +0300

    Update ذك.py

--- zthon/plugins/ذك.py
+++ zthon/plugins/ذك.py
@@ -1,53 +1,146 @@
-from somnium import Somnium
+""" 
+OpenAi & ChatGpt for Tepthon™ t.me/Tepthon
+Write file by MUHAMMAD t.me/PPF22
+ها خماط بعدك تخمط مني .. ماتستحي ؟
+متى راح تصير مطور وانت مقضيها خمط تعب وحقوق الناس
+ههههههههههههههههههههههههههههههههههههههههههههههههههههههه
+"""
 
-from ..core.managers import edit_delete, edit_or_reply
-from ..helpers import GetStylesGraph, reply_id
-from ..sql_helper.globals import addgvar, gvarstatus
+import requests
+import asyncio
+import os
+import sys
+import urllib.request
+from datetime import timedelta
+from telethon import events
+from telethon.errors import FloodWaitError
+from telethon.tl.functions.messages import GetHistoryRequest, ImportChatInviteRequest
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.messages import ImportChatInviteRequest
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.contacts import UnblockRequest as unblock
+from telethon.tl.functions.messages import ImportChatInviteRequest as Get
+
+from zthon import zedub
 from . import zedub
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
+from ..helpers.utils import reply_id
+
+plugin_category = "البوت"
 
 
-@zedub.zed_cmd(
-    pattern="ذك(?:\s|$)([\s\S]*)",
-)
-async def ai_img(odi):
-    reply_to_id = await reply_id(odi)
-    query = odi.pattern_match.group(1)
-    if not query:
-        return await edit_delete(odi, "**⎆ عـذرًا عـــزيـزي يـجـب تـحـديـد نوع الصـورة أولًا**")
+# code by t.me/zzzzl1l
+@zedub.zed_cmd(pattern="تيب(?: |$)(.*)")
+async def zelzal_gpt(event):
+    zilzal = event.pattern_match.group(1)
+    zzz = await event.get_reply_message()
+    chat = "@GPT4Telegrambot" #code by t.me/zzzzl1l
+    if not zilzal and not event.reply_to_msg_id:
+        return await edit_or_reply(event, "**⎉╎بالـرد ع سـؤال او باضـافة السـؤال للامـر**\n**⎉╎مثـــال :**\n`.تيب من هو مكتشف الجاذبية الارضية`")
+    if not zilzal and event.reply_to_msg_id and zzz.text: #code by t.me/zzzzl1l
+        zelzal = zzz.text
+    if not event.reply_to_msg_id: #code by t.me/zzzzl1l
+        zelzal = event.pattern_match.group(1)
+    zed = await edit_or_reply(event, "**⎉╎جـارِ الاتصـال بـ الذكـاء الاصطنـاعـي\n⎉╎الرجـاء الانتظـار .. لحظـات**")
+    async with borg.conversation(chat) as conv: #code by t.me/zzzzl1l
+        try:
+            await conv.send_message(zelzal)
+            zzzthon = await conv.get_response()
+            ahmed = zzzthon.text
+            if "another 8 seconds" in zzzthon.text: #code by t.me/zzzzl1l
+                aa = ahmed.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**⎉╎يُرجى الانتظار 8 ثوانٍ ⏳\n⎉╎بين ارسـال كل سـؤال والتـالي**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await asyncio.sleep(5)
+            zedthon = await conv.get_response()
+            malath = zedthon.text
+            if "understanding" in zedthon.text: #code by t.me/zzzzl1l
+                aa = malath.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**- عـذراً .. لم أفهم سؤالك\n- قم بـ إعادة صياغته من فضلك؟!**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await zed.delete()
+            await borg.send_message(event.chat_id, f"**س/ {zelzal}\n\n{malath}**\n\n───────────────────\n𝗭𝗧𝗵𝗼𝗻 𝗨**ꜱᴇʀʙᴏᴛ** 𝗧**ᴏᴏʟꜱ**\n\t\t\t\t\t\t\t\tᶻᵉˡᶻᵃˡ • ᴼᵖᵉⁿᴬᴵ")
+        except YouBlockedUserError: #code by t.me/zzzzl1l
+            await zedub(unblock("GPT4Telegrambot"))
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.send_message(zelzal)
+            zzzthon = await conv.get_response()
+            ahmed = zzzthon.text
+            if "another 8 seconds" in zzzthon.text: #code by t.me/zzzzl1l
+                aa = ahmed.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**⎉╎يُرجى الانتظار 8 ثوانٍ ⏳\n⎉╎بين ارسـال كل سـؤال والتـالي**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await asyncio.sleep(5)
+            zedthon = await conv.get_response()
+            malath = zedthon.text
+            if "understanding" in zedthon.text: #code by t.me/zzzzl1l
+                aa = malath.replace("I'm sorry, I'm not quite understanding the question. Could you please rephrase it?", "**- عـذراً .. لم أفهم سؤالك\n- قم بـ إعادة صياغته من فضلك؟!**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            if "Please wait a moment" in zedthon.text: #code by t.me/zzzzl1l
+                await asyncio.sleep(5)
+                zedthon = await conv.get_response()
+                malath = zedthon.text
+            await zed.delete()
+            await borg.send_message(event.chat_id, f"**س/ {zelzal}\n\n{malath}**\n\n───────────────────\n𝗭𝗧𝗵𝗼𝗻 𝗨**ꜱᴇʀʙᴏᴛ** 𝗧**ᴏᴏʟꜱ**\n\t\t\t\t\t\t\t\tᶻᵉˡᶻᵃˡ • ᴼᵖᵉⁿᴬᴵ")
 
-    moevent = await edit_or_reply(odi, "**- جار الان الصنع يرجى الانتظار قليلا**")
-    rstyles = {value: key for key, value in Somnium.Styles().items()}
-    styleid = int(gvzedstatus("DREAM_STYLE") or "84")
 
-    if query.stzedtswith("النوع"):
-        query = query.replace("النوع", "").strip()
-        if query.isnumeric():
-            if int(query) in rstyles:
-                addgvzed("DREAM_STYLE", int(query))
-                return await edit_delete(
-                    moevent, f"⎆ تم بـنـجـاح تـغـيـيـر النوع إلـى {rstyles[int(query)]}."
-                )
+# تخمــط اهينـــك Fuk-You
 
-            return await edit_delete(
-                moevent,
-                f"يجب اختيار النوع بشكل صحيح\n\nهذه هي قائمة الأنواع :  [اضغط هنا]({await GetStylesGraph()}) ",
-                link_preview=True,
-                time=120,
-            )
-
-        return await edit_delete(
-            moevent,
-            f"أهـلًا عـزيـزي {mention} هـذه هـي قـائـمة الأنـواع [اضغط هنا]({await GetStylesGraph()}) ",
-            link_preview=True,
-            time=120,
-        )
-    await edit_or_reply(moevent, "⎆ يـرجـى الانـتـظـار قـليـلًا .")
-    getart = Somnium.Generate(query, styleid)
-    await zedub.send_file(
-        odi.chat_id,
-        getart,
-        force_document=True,
-        reply_to=reply_to_id,
-        caption=f"الـعنوان: {query}\nالنوع: {rstyles[styleid]}\n\n@Tepthon فـريـق تيـبـثـون الـعــربـي",
-    )
-    await moevent.delete()
+# code by t.me/zzzzl1l
+@zedub.zed_cmd(pattern="س(?: |$)(.*)")
+async def zelzal_gpt(event):
+    zilzal = event.pattern_match.group(1)
+    zzz = await event.get_reply_message()
+    chat = "@GPT4Telegrambot" #code by t.me/zzzzl1l
+    if not zilzal and not event.reply_to_msg_id:
+        return await edit_or_reply(event, "**⎉╎بالـرد ع سـؤال او باضـافة السـؤال للامـر**\n**⎉╎مثـــال :**\n`.زد من هو مكتشف الجاذبية الارضية`")
+    if not zilzal and event.reply_to_msg_id and zzz.text: #code by t.me/zzzzl1l
+        zelzal = zzz.text
+    if not event.reply_to_msg_id: #code by t.me/zzzzl1l
+        zelzal = event.pattern_match.group(1)
+    zed = await edit_or_reply(event, "**⎉╎جـارِ الاتصـال بـ الذكـاء الاصطنـاعـي\n⎉╎الرجـاء الانتظـار .. لحظـات**")
+    async with borg.conversation(chat) as conv: #code by t.me/zzzzl1l
+        try:
+            await conv.send_message(zelzal)
+            zzzthon = await conv.get_response()
+            ahmed = zzzthon.text
+            if "another 8 seconds" in zzzthon.text: #code by t.me/zzzzl1l
+                aa = ahmed.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**⎉╎يُرجى الانتظار 8 ثوانٍ ⏳\n⎉╎بين ارسـال كل سـؤال والتـالي**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await asyncio.sleep(5)
+            zedthon = await conv.get_response()
+            malath = zedthon.text
+            if "understanding" in zedthon.text: #code by t.me/zzzzl1l
+                aa = malath.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**- عـذراً .. لم أفهم سؤالك\n- قم بـ إعادة صياغته من فضلك؟!**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            if "Please wait a moment" in zedthon.text: #code by t.me/zzzzl1l
+                await asyncio.sleep(5)
+                zedthon = await conv.get_response()
+                malath = zedthon.text
+            await zed.delete()
+            await borg.send_message(event.chat_id, f"**س/ {zelzal}\n\n{malath}**\n\n───────────────────\n𝗭𝗧𝗵𝗼𝗻 𝗨**ꜱᴇʀʙᴏᴛ** 𝗧**ᴏᴏʟꜱ**\n\t\t\t\t\t\t\t\tᶻᵉˡᶻᵃˡ • ᴼᵖᵉⁿᴬᴵ")
+        except YouBlockedUserError: #code by t.me/zzzzl1l
+            await zedub(unblock("GPT4Telegrambot"))
+            await conv.send_message("/start")
+            await conv.get_response()
+            await conv.send_message(zelzal)
+            zzzthon = await conv.get_response()
+            ahmed = zzzthon.text
+            if "another 8 seconds" in zzzthon.text: #code by t.me/zzzzl1l
+                aa = ahmed.replace("⏳ Please wait another 8 seconds before sending the next question . . .", "**⎉╎يُرجى الانتظار 8 ثوانٍ ⏳\n⎉╎بين ارسـال كل سـؤال والتـالي**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await asyncio.sleep(5)
+            zedthon = await conv.get_response()
+            malath = zedthon.text
+            if "understanding" in zedthon.text: #code by t.me/zzzzl1l
+                aa = malath.replace("I'm sorry, I'm not quite understanding the question. Could you please rephrase it?", "**- عـذراً .. لم أفهم سؤالك\n- قم بـ إعادة صياغته من فضلك؟!**") 
+                await event.delete()
+                return await borg.send_message(event.chat_id, aa)
+            await zed.delete()
+            await borg.send_message(event.chat_id, f"**س/ {zelzal}\n\n{malath}**\n\n───────────────────\n𝗭𝗧𝗵𝗼𝗻 𝗨**ꜱᴇʀʙᴏᴛ** 𝗧**ᴏᴏʟꜱ**\n\t\t\t\t\t\t\t\tᶻᵉˡᶻᵃˡ • ᴼᵖᵉⁿᴬᴵ")
