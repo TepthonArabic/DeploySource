09e0c1f98f602ebb350dd2d200ca04e3ab3203d9
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-06-18 16:26:39 +0300

    update inlinebot.py

--- zthon/core/inlinebot.py
+++ zthon/core/inlinebot.py
@@ -317,7 +317,7 @@ async def inline_handler(event):  # sourcery no-metrics
                 Button.url("قنـاة السـورس", "https://t.me/Tepthon"),
                 Button.url(
                     "مطـور السـورس",
-                    "https://t.me/PPF22",
+                    "https://t.me/E_2_D",
                 ),
             )
         ]
