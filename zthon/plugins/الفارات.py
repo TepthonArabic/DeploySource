3f20805b7cbf9d51b130c3bb60b6a3cc677e1ca6
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-07-01 19:30:53 +0300

    update الفارات.py

--- zthon/plugins/الفارات.py
+++ zthon/plugins/الفارات.py
@@ -29,7 +29,7 @@ from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_display_name
 from urlextract import URLExtract
 
-from Zara import zedub
+fromzthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
