a2c42c03d8431b4af8be5f515d0fc7b9a2b91ea2
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-07-17 21:45:51 +0300

    update bot_pms.py

--- zthon/assistant/bot_pms.py
+++ zthon/assistant/bot_pms.py
@@ -113,7 +113,7 @@ async def bot_start(event):
                 Button.url("قنـاة السـورس", "https://t.me/Tepthon"),
                 Button.url(
                     "مطـور السـورس",
-                    "https://t.me/R_6_B",
+                    "https://t.me/W_4_7",
                 ),
             )
         ]
