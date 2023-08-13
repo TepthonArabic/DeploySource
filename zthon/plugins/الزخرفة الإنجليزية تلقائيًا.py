7b5f1f9260728e37fc4c1aff4e6ac3a0c2c03fab
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-08-13 15:46:43 +0300

    update الزخرفة الإنجليزية تلقائيًا.py

--- zthon/plugins/الزخرفة الإنجليزية تلقائيًا.py
+++ zthon/plugins/الزخرفة الإنجليزية تلقائيًا.py
@@ -19,7 +19,7 @@ async def zakrafaon(event):
 @zedub.zed_cmd(pattern="ايقاف الزخرفة الانجليزية")
 async def zakrafaoff(event):
     if not gvarstatus("enzakrafa"):
-        await edit_delete(event, "*⪼ عـذرًا عـزيـزي أنـت لـم تقـم بتـعطيل الزخـرفـة الإنجلـيزية تلقـائيًا**")
+        await edit_delete(event, "*⪼ عـذرًا عـزيـزي أنـت لـم تقـم بتفعيـل الزخـرفـة الإنجلـيزية تلقـائيًا**")
         return
     if gvarstatus("enzakrafa"):
         delgvar("enzakrafa")
@@ -58,6 +58,61 @@ async def zakrafarun(event):
             .replace("x", "𝘅")
             .replace("y", "𝘆")
             .replace("z", "𝘇")
+        )
+        await event.edit(uppercase_text)
+@zedub.zed_cmd(pattern="زخرفة 2")
+async def zakrafaon(event):
+    if not gvarstatus("enzakrafatwo"):
+        addgvar("enzakrafatwo", "on")
+        await edit_delete(event, "**⪼ تـم تـفعـيل الزخـرفـة الإنـجليـزيـة 2**")
+        return
+    if gvarstatus("enzakrafatwo"):
+        await edit_delete(event, "**⪼ الزخـرفـة الإنـجـليزيـة 2 مفعلة مسبقًا .**")
+        return
+
+
+@zedub.zed_cmd(pattern="ايقاف الزخرفة 2")
+async def zakrafaoff(event):
+    if not gvarstatus("enzakrafatwo"):
+        await edit_delete(event, "*⪼ عـذرًا عـزيـزي أنـت لـم تقـم بتفعيـل الزخـرفـة الإنجلـيزية 2*")
+        return
+    if gvarstatus("enzakrafatwo"):
+        delgvar("enzakrafatwo")
+        await edit_delete(event, "**⪼ تـم تـعطـيل الزخرفـة الإنـجليـزية 2**")
+        return
+
+
+@zedub.on(events.NewMessage(outgoing=True))
+async def zakrafarun(event):
+    if gvarstatus("enzakrafatwo"):
+        text = event.message.message
+        uppercase_text = (
+            text.replace("a", "ꪖ")
+            .replace("b", "Ⴆ")
+            .replace("c", "ᥴ")
+            .replace("d", "ᦔ")
+            .replace("e", "꧖")
+            .replace("f", "ƒ")
+            .replace("g", "ᧁ")
+            .replace("h", "ꫝ")
+            .replace("i", "Ꭵ")
+            .replace("j", "᧒")
+            .replace("k", "ƙ")
+            .replace("l", "ᥣ")
+            .replace("m", "᧗")
+            .replace("n", "ᥒ")
+            .replace("o", "᥆")
+            .replace("p", "ρ")
+            .replace("q", "ᑫ")
+            .replace("r", "ᖇ")
+            .replace("s", "᥉")
+            .replace("t", "ﾋ")
+            .replace("u", "ꪊ")
+            .replace("v", "ꪜ ")
+            .replace("w", "ꪝ")
+            .replace("x", "ꪎ")
+            .replace("y", "ꪗ")
+            .replace("z", "ᤁ")
         )
         await event.edit(uppercase_text)
         #مأخوذ_من_سورس_جمثون
