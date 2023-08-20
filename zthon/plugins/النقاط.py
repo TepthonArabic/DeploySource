e443b7d7cb59f176e110049218258519c529f584
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-08-20 20:09:46 +0300

    update النقاط.py

--- zthon/plugins/النقاط.py
+++ zthon/plugins/النقاط.py
@@ -296,6 +296,103 @@ async def _(event):
 
 
 
+@zedub.zed_cmd(pattern="بوت دعمكم$")
+async def _(event):
+    await event.edit('@DamKombot')
+
+# Copyright (C) 2022 Zed-Thon . All Rights Reserved
+@zedub.zed_cmd(pattern="دعمكم ?(.*)")
+async def _(event):
+    con = event.pattern_match.group(1).lower()
+    await event.edit("**𓆰 حـسنـًا .. تأكـد من انك مشتـرك بـ قنـوات الاشتـراك الاجبـاري لتجنب الأخطـاء @DamKombot**")
+    channel_entity = await zedub.get_entity('@DamKombot')
+    await zedub.send_message('@DamKombot', '/start')
+    await asyncio.sleep(4)
+    msg0 = await zedub.get_messages('@DamKombot', limit=1)
+    await msg0[0].click(2)
+    await asyncio.sleep(4)
+    msg1 = await zedub.get_messages('@DamKombot', limit=1)
+    await msg1[0].click(0)
+
+    chs = 1
+    for i in range(100):
+        await asyncio.sleep(4)
+
+        list = await zedub(GetHistoryRequest(peer=channel_entity, limit=1,
+                                               offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('**𓆰 لا يوجد قنوات في الوقت الحالي .. قم يتجميع النقاط بطريقـة مـخـتـلفة**') != -1:
+            await zedub.send_message(event.chat_id, f"**𓆰 لا يـوجـد قنـوات بالبـوت حـاليًا ...**")
+            break
+        if con == "ايقاف":
+            await zedub.send_message(event.chat_id, f"**𓆰 تم إيقـاف تجميـع النقـاط ☑️ ...**")
+            break
+        url = msgs.reply_markup.rows[0].buttons[0].url
+        try:
+            try:
+                await zedub(JoinChannelRequest(url))
+            except:
+                bott = url.split('/')[-1]
+                await zedub(ImportChatInviteRequest(bott))
+            msg2 = await zedub.get_messages('@DamKombot', limit=1)
+            await msg2[0].click(text='تحقق')
+            chs += 1
+            await event.edit(f"**𓆰 تم بنجـاح الاشتـراك في {chs} قنـاة .❗**")
+        except:
+            msg2 = await zedub.get_messages('@DamKombot', limit=1)
+            await msg2[0].click(text='التالي')
+            chs += 1
+            await event.edit(f"**𓆰 القنـاة رقـم {chs} خطـأ .. يمكـن تبنـدت**")
+    await zedub.send_message(event.chat_id, "**𓆰 تم الانتهـاء مـن تجميـع النقـاط .. حاول من جديد في وقت آخر ✓**")
+
+
+
+@zedub.zed_cmd(pattern="تجميع دعمكم ?(.*)")
+async def _(event):
+    con = event.pattern_match.group(1).lower()
+    await event.edit("**𓆰 حـسنـًا .. تأكـد من انك مشتـرك بـ قنـوات الاشتـراك الاجبـاري لتجنب الأخطـاء @DamKombot**")
+    channel_entity = await zedub.get_entity('@DamKombot')
+    await zedub.send_message('@DamKombot', '/start')
+    await asyncio.sleep(4)
+    msg0 = await zedub.get_messages('@DamKombot', limit=1)
+    await msg0[0].click(2)
+    await asyncio.sleep(4)
+    msg1 = await zedub.get_messages('@DamKombot', limit=1)
+    await msg1[0].click(0)
+
+    chs = 1
+    for i in range(100):
+        await asyncio.sleep(4)
+
+        list = await zedub(GetHistoryRequest(peer=channel_entity, limit=1,
+                                               offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('**𓆰 لا يوجد قنوات في الوقت الحالي .. قم يتجميع النقاط بطريقـة مـخـتـلفة**') != -1:
+            await zedub.send_message(event.chat_id, f"**𓆰 لا يـوجـد قنـوات بالبـوت حـاليًا ...**")
+            break
+        if con == "ايقاف":
+            await zedub.send_message(event.chat_id, f"**𓆰 تم إيقـاف تجميـع النقـاط ☑️ ...**")
+            break
+        url = msgs.reply_markup.rows[0].buttons[0].url
+        try:
+            try:
+                await zedub(JoinChannelRequest(url))
+            except:
+                bott = url.split('/')[-1]
+                await zedub(ImportChatInviteRequest(bott))
+            msg2 = await zedub.get_messages('@DamKombot', limit=1)
+            await msg2[0].click(text='تحقق')
+            chs += 1
+            await event.edit(f"**𓆰 تم بنجـاح الاشتـراك في {chs} قنـاة .❗**")
+        except:
+            msg2 = await zedub.get_messages('@DamKombot', limit=1)
+            await msg2[0].click(text='التالي')
+            chs += 1
+            await event.edit(f"**𓆰 القنـاة رقـم {chs} خطـأ .. يمكـن تبنـدت**")
+    await zedub.send_message(event.chat_id, "**𓆰 تم الانتهـاء مـن تجميـع النقـاط .. حاول من جديد في وقت آخر ✓**")
+    
+    
+ 
 # code by @r0r77 & @Dar4k
 @zedub.zed_cmd(pattern="بخشيش وعد (.*)")
 async def baqshis(event):
