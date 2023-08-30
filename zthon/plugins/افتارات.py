976d8274d7ac25fbaa06a5d0abe2cd9f1a73ef41
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-08-30 18:12:53 +0300

    update افتارات.py

--- zthon/plugins/افتارات.py
+++ zthon/plugins/افتارات.py
@@ -380,27 +380,6 @@ async def _(event):
         await zzevent.edit("**╮•⎚ عـذرًا .. لـم استطـع ايجـاد المطلـوب ☹️💔**")
 
 
-@zedub.zed_cmd(pattern="$صورة تمبلر")
-async def _(event):
-    zzevent = await edit_or_reply(event, "**╮ - جـارِ العثـور علـى صـور تمبلـر انتظـر ..**")
-    try:
-        zedph = [
-            zelzal
-            async for zelzal in event.client.iter_messages(
-                "@N1NNNN", filter=InputMessagesFilterPhotos
-            )
-        ]
-        aing = await event.client.get_me()
-        await event.client.send_file(
-            event.chat_id,
-            file=random.choice(zedph),
-            caption=f"**◞صور تمبلـر ✨**\n\n[➧𝙎𝙊𝙐𝙍𝘾𝙀 𝙏𝙀𝙋𝙏𝙃𝙊𝙉](https://t.me/Tepthon)",
-        )
-        await zzevent.delete()
-    except Exception:
-        await zzevent.edit("**╮•⎚ عـذرًا .. لـم استطـع ايجـاد المطلـوب ☹️💔**")
-        
-        
 @zedub.zed_cmd(pattern="بنت انمي$")
 async def _(event):
     zzevent = await edit_or_reply(event, "**╮ - جـارِ تحميـل الآفتـار ...𓅫╰**")
@@ -416,6 +395,26 @@ async def _(event):
             event.chat_id,
             file=random.choice(zedph),
             caption=f"**◞افتـارات آنمي بنـات ➧🎆🧚🏻‍♀◟**\n\n[➧𝙎𝙊𝙐𝙍𝘾𝙀 𝙏𝙀𝙋𝙏𝙃𝙊𝙉](https://t.me/Tepthon)",
+        )
+        await zzevent.delete()
+    except Exception:
+        await zzevent.edit("**╮•⎚ عـذرًا .. لـم استطـع ايجـاد المطلـوب ☹️💔**")
+
+@zedub.zed_cmd(pattern="$صورة تمبلر")
+async def _(event):
+    zzevent = await edit_or_reply(event, "**╮ - جـارِ العثـور علـى صـور تمبلـر انتظـر ..**")
+    try:
+        zedph = [
+            zelzal
+            async for zelzal in event.client.iter_messages(
+                "@N1NNNN", filter=InputMessagesFilterPhotos
+            )
+        ]
+        aing = await event.client.get_me()
+        await event.client.send_file(
+            event.chat_id,
+            file=random.choice(zedph),
+            caption=f"**◞صور تمبلـر ✨**\n\n[➧𝙎𝙊𝙐𝙍𝘾𝙀 𝙏𝙀𝙋𝙏𝙃𝙊𝙉](https://t.me/Tepthon)",
         )
         await zzevent.delete()
     except Exception:
