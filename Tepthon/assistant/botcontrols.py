9533dad0c93c76f05f07d9c749d7eb5aba8da229
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-30 18:38:49 +0300

    Update botcontrols.py

--- Tepthon/assistant/botcontrols.py
+++ Tepthon/assistant/botcontrols.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from telethon.errors import BadRequestError, FloodWaitError, ForbiddenError
 
-from zthon import zedub
+from Tepthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
