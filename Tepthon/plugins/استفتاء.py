b65e9c6274f0fb0b159cfa2f52ec7808cfbc6f61
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-30 19:30:33 +0300

    Update استفتاء.py

--- Tepthon/plugins/استفتاء.py
+++ Tepthon/plugins/استفتاء.py
@@ -4,7 +4,7 @@ from telethon.errors.rpcbaseerrors import ForbiddenError
 from telethon.errors.rpcerrorlist import PollOptionInvalidError
 from telethon.tl.types import InputMediaPoll, Poll
 
-from zthon import zedub
+from Tepthon import zedub
 
 from ..core.managers import edit_or_reply
 from . import Build_Poll, reply_id
