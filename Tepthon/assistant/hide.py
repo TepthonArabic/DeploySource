dcd47de490bd8ac675aa31001d71d812170ed082
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-30 18:39:19 +0300

    Update hide.py

--- Tepthon/assistant/hide.py
+++ Tepthon/assistant/hide.py
@@ -4,14 +4,14 @@ import re
 
 from telethon.events import CallbackQuery
 
-from zthon import zedub
+from Tepthon import zedub
 
 
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./zthon/hide.txt"):
-        jsondata = json.load(open("./zthon/hide.txt"))
+    if os.path.exists("./Tepthon/hide.txt"):
+        jsondata = json.load(open("./Tepthon/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
