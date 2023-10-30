97dbb141c080f97270dc8d5239827d4806ece933
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-10-30 18:44:58 +0300

    Update chatbot.py

--- Tepthon/helpers/chatbot.py
+++ Tepthon/helpers/chatbot.py
@@ -3,8 +3,8 @@ import re
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest as unblock
 
-from zthon import zedub
-from zthon.helpers.functions import delete_conv
+from Tepthon import zedub
+from Tepthon.helpers.functions import delete_conv
 
 from ..sql_helper.globals import addgvar, gvarstatus
 
