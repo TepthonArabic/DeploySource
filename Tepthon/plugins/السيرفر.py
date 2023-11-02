de9011512e3f734864db3bd4fe383a5050b528f5
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-02 14:45:51 +0300

    Update السيرفر.py

--- Tepthon/plugins/السيرفر.py
+++ Tepthon/plugins/السيرفر.py
@@ -7,7 +7,7 @@ import asyncio
 import glob
 import os
 
-from zthon import zedub
+from Tepthon import zedub
 
 from ..core.managers import edit_delete, edit_or_reply
 from ..helpers.utils import _zedutils
