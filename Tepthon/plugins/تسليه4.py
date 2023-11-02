e279b7e5c12113182a053a7b4e90e844cfe6c9ce
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-02 15:11:34 +0300

    Update تسليه4.py

--- Tepthon/plugins/تسليه4.py
+++ Tepthon/plugins/تسليه4.py
@@ -5,8 +5,8 @@ import os
 import random
 from urllib.parse import quote_plus
 from collections import deque
-from zthon.core.logger import logging
-from zthon import zedub
+from Tepthon.core.logger import logging
+from Tepthon import zedub
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from . import ALIVE_NAME, deEmojify, mention
