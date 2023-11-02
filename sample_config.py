1a46467706e249b055f5e55b91b172d0f12e30fd
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-03 01:12:23 +0300

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Tepthonee/PPF22"
+        "UPSTREAM_REPO", "https://github.com/Tepthonee/thesource"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
