48fc2d42f99d6c26ddcf277468ae584e9352b82a
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-02 14:44:31 +0300

    Update الحمايه.py

--- Tepthon/plugins/الحمايه.py
+++ Tepthon/plugins/الحمايه.py
@@ -6,8 +6,8 @@ from telethon import Button, functions
 from telethon.events import CallbackQuery
 from telethon.utils import get_display_name
 
-from zthon import zedub
-from zthon.core.logger import logging
+from Tepthon import zedub
+from Tepthon.core.logger import logging
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
