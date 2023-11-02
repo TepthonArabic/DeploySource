131659015063b3c403817e7883f0d7c742c015dd
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-02 15:07:57 +0300

    Update بلاي.py

--- Tepthon/plugins/بلاي.py
+++ Tepthon/plugins/بلاي.py
@@ -1,5 +1,5 @@
-from zthon import zedub
-from zthon.core.logger import logging
+from Tepthon import zedub
+from Tepthon.core.logger import logging
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
