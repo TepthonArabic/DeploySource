a2e931556547f2e96f4dbdb73365c1100f9a3ab7
BiLaL <97440505+E9N99@users.noreply.github.com>
2023-11-02 14:52:09 +0300

    Update النشر التلقائي.py

--- Tepthon/plugins/النشر التلقائي.py
+++ Tepthon/plugins/النشر التلقائي.py
@@ -19,12 +19,12 @@ from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl.functions.messages import ExportChatInviteRequest
 from telethon.tl.functions.users import GetFullUserRequest
 
-from zthon import zedub
+from Tepthon import zedub
 
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from ..sql_helper.autopost_sql import add_post, get_all_post, is_post, remove_post
-from zthon.core.logger import logging
+from Tepthon.core.logger import logging
 from ..sql_helper.globals import gvarstatus
 from . import BOTLOG, BOTLOG_CHATID
 from . import *
