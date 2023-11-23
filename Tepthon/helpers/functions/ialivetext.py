fefbd4f44f16ce538d7e9a7efe00bccbc2818064
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-11-23 10:53:39 +0300

    Update ialivetext.py

--- Tepthon/helpers/functions/ialivetext.py
+++ Tepthon/helpers/functions/ialivetext.py
@@ -83,7 +83,7 @@ async def zedalive(StartTime):
         dyno = f"{AppHours}h {AppMinutes}m/{hours}h {minutes}m"
     except Exception as e:
         dyno = e
-    return f"🖤༄ ZThon Stats ༄🖤\
+    return f"🖤༄ Tepthon Stats ༄🖤\
                  \n\nღ Database : {check_sgnirts}\
                   \nღ Sudo : {sudo}\
                   \nღ Uptime : {uptime}\
