d9db1714fdcf5125f8797b187c10a840e37537d7
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-11-23 08:18:51 +0300

    update فحص فيزات.py

--- Tepthon/plugins/فحص فيزات.py
+++ Tepthon/plugins/فحص فيزات.py
@@ -80,7 +80,33 @@ async def song2(event):
         await event.client.send_message(event.chat_id, response.message)
         await zed.delete()
         
-        
+ 
+# code by t.me/zzzzl1l
+@zedub.zed_cmd(pattern="تاريخ انشاء الحساب(?:\s|$)([\s\S]*)")
+async def song2(event):
+    song = event.pattern_match.group(1)
+    chat = "@inf0tlebot" # code by t.me/zzzzl1l
+    reply_id_ = await reply_id(event)
+    zed = await edit_or_reply(event, "**⎉╎جـارِ معرفـة تاريـخ إنشاء حسابـك**")
+    async with event.client.conversation(chat) as conv:
+        try:
+            gool = "/start {}".format(song)
+            await conv.send_message(gool)
+        except YouBlockedUserError:
+            await zedub(unblock("inf0tlebot"))
+            gool = "/start {}".format(song)
+            await conv.send_message(gool)
+        await asyncio.sleep(22)
+        response = await conv.get_response()
+        if response.text.startswith("ANTI_SPAM:"):
+        	return await zed.edit("**- حاول مجـدداً ولا تستخـدم سبـام ...**")
+        if response.text.startswith("RISK:"):
+        	return await zed.edit("**- خطـأ :**\n**أعد محاولة فحص هذه البطاقه ...لاحقًا**")
+        await event.client.send_read_acknowledge(conv.chat_id)
+        await event.client.send_message(event.chat_id, response.message)
+        await zed.delete()
+ 
+ 
 # code by t.me/zzzzl1l
 @zedub.zed_cmd(pattern="كومبو(?:\s|$)([\s\S]*)")
 async def song2(event): # code by t.me/zzzzl1l
