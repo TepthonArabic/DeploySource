13176c5f337a9300ceed6c9b9dca2167495cc276
روجر <104784913+rogerpq@users.noreply.github.com>
2023-12-02 15:51:42 +0300

    Update zedub_config.py

--- Tepthon/Config/zedub_config.py
+++ Tepthon/Config/zedub_config.py
@@ -8,3 +8,5 @@ if ENV:
     from sample_config import Config  # noqa
 elif os.path.exists("config.py"):
     from config import Development as Config  # noqa
+else:
+    from sample_config import Config #noqa
