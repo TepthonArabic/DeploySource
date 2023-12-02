b83756e7b1336be79a0d170623554efc01c8689b
روجر <104784913+rogerpq@users.noreply.github.com>
2023-12-02 16:30:10 +0300

    Update __main__.py

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -1,17 +1,19 @@
-from telethon.tl.functions.messages import GetMessagesViewsRequest
-import sys, asyncio
+import sys
 import Tepthon
-from Tepthon import zedub
-from Tepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
-from telethon import functions
+from Tepthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, autovars, autoname
-from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+from .utils import mybot, autoname, autovars
+from .utils import (
+    add_bot_to_logger_group,
+    load_plugins,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
 
 LOGS = logging.getLogger("سورس تيبثون")
-cmdhr = Config.COMMAND_HAND_LER
 
 print(Tepthon.__copyright__)
 print(f"المرخصة بموجب شروط  {Tepthon.__license__}")
@@ -19,96 +21,57 @@ print(f"المرخصة بموجب شروط  {Tepthon.__license__}")
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("✧ جـاري تحميـل الملحقـات ✧")
+    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
     zedub.loop.run_until_complete(autovars())
     LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
 except Exception as e:
-    LOGS.error(f"{str(e)}")
+    LOGS.error(f"{e}")
     sys.exit()
 
-class CatCheck:
-    def __init__(self):
-        self.sucess = True
-Catcheck = CatCheck()
+if not Config.ALIVE_NAME:
+    try: #Code by T.me/E_7_V
+        LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
+        zedub.loop.run_until_complete(autoname())
+        LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
+    except Exception as e:
+        LOGS.error(f"- {e}")
 
-try:
-    LOGS.info("✧ بـدء اضـافـة الاسـم الـتـلقـائي ✧")
-    zedub.loop.run_until_complete(autoname())
-    LOGS.info("✓ تـم اضـافـة الاسـم الـتـلقـائي .. بـنجـاح ✓")
-except Exception as e:
-    LOGS.error(f"- {e}")
-
-
-try:
-    LOGS.info("✧ جـاري تـفـعـيل وضـع الانـلاين ✧")
-    zedub.loop.create_task(mybot())
-    LOGS.info("✓ تـم تـفـعـيل وضع الانـلاين .. بنجـاح ✓")
+try: #Code by T.me/E_7_V
+    LOGS.info("⌭ بـدء تنزيـل تيـبثون ⌭")
+    zedub.loop.run_until_complete(setup_bot())
+    LOGS.info("⌭ بـدء تشغيل البـوت ⌭")
 except Exception as e:
     LOGS.error(f"- {e}")
 
 try:
-    LOGS.info("✧ بـدء تـنزيـل تيـبثون ✧")
-    zedub.loop.create_task(setup_bot())
-    LOGS.info("✧ تم تنزيل تيـبثون ✧")
+    LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
+
 
 
 async def startup_process():
-    async def MarkAsViewed(channel_id):
-        from telethon.tl.functions.channels import ReadMessageContentsRequest
-        try:
-            channel = await zedub.get_entity(channel_id)
-            async for message in zedub.iter_messages(entity=channel.id, limit=5):
-                try:
-                    await zedub(GetMessagesViewsRequest(peer=channel.id, id=[message.id], increment=True))
-                except Exception as error:
-                    print ("✅")
-            return True
-
-        except Exception as error:
-            print ("✅")
-
-    async def start_bot():
-      try:
-          List = ["Tepthon","PPYNY","Tepthone1","Tws_Tepthon","Tepthon_Help","super_tepthon","tepthonklaesh","x_wwl","K_K_Q_L","VisaTepthon","zszxzz","TIKTOK_ased","huueuu","PyThOnX1"]
-          from telethon.tl.functions.channels import JoinChannelRequest
-          for id in List :
-              Join = await zedub(JoinChannelRequest(channel=id))
-              MarkAsRead = await MarkAsViewed(id)
-              print (MarkAsRead, "✅")
-          return True
-      except Exception as e:
-        print("✅")
-        return False
-    
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"⌔┊تـم تنصيـب تيبــثون . . بنجـاح ✓ \n⌔┊لـ إظهـار الاوامـر ارسـل (.الاوامر)")
+    print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
+    print("تـم التنصـيب .. بنجـاح ✓")
+    print(
+        f"⌔┊تـم تنصيـب ريبثون يـوزربـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  حمد\n⌔┊قنـاة السـورس ↶.\n🌐┊@Tepthon"
+    )
+    print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    Catcheck.sucess = True
-    
-    Checker = await start_bot()
-    if Checker == False:
-        print("#1")
-    else:
-        print ("✅")
-    
     return
 
-
 zedub.loop.run_until_complete(startup_process())
-
 if len(sys.argv) not in (1, 3, 4):
     zedub.disconnect()
-elif not Catcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
 else:
     try:
         zedub.run_until_disconnected()
