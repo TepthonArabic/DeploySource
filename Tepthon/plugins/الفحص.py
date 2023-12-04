dc7ba14599ad9b18caa2c24ce217f6d4c1954713
روجر <104784913+rogerpq@users.noreply.github.com>
2023-12-04 18:57:32 +0300

    Update الفحص.py

--- Tepthon/plugins/الفحص.py
+++ Tepthon/plugins/الفحص.py
@@ -3,6 +3,7 @@ import re
 import time
 from datetime import datetime
 from platform import python_version
+from random import choice
 
 import requests
 from telethon import version
@@ -13,7 +14,7 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from Tepthon import StartTime, zedub, zedversion
+from Tepthon import StartTime, zedub, tepversion
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
@@ -23,7 +24,73 @@ from ..sql_helper.globals import gvarstatus
 from . import mention
 
 plugin_category = "العروض"
-STATS = gvarstatus("Z_STATS") or "فحص"
+STATS = gvarstatus("T_STATS") or "فحص"
+
+# @E_7_V 
+file_path = "installation_date.txt"
+if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+    with open(file_path, "r") as file:
+        installation_time = file.read().strip()
+else:
+    installation_time = datetime.now().strftime("%Y-%m-%d")
+    with open(file_path, "w") as file:
+        file.write(installation_time)
+
+@zedub.zed_cmd(pattern=f"{STATS}$")
+async def amireallyalive(event):
+    reply_to_id = await reply_id(event)
+    uptime = await get_readable_time((time.time() - StartTime))
+    start = datetime.now()
+    tepevent = await edit_or_reply(event, "**⛥ ⤻ انتـظࢪ جـاࢪي فـحص بـ ـوت TEᑭTᕼOᑎ الخـاص بـِك   ۦ**")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    _, check_sgnirts = check_data_base_heal_th()
+    RANDOM_MEDIA = ["https://graph.org/file/6fa698c2e409a2bf41bd6.mp4", "https://graph.org/file/1131ec8fee887ddcc06bd.mp4"]
+    T_EMOJI = gvarstatus("ALIVE_EMOJI") or "☼ ⤶"
+    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ☼ TEᑭTᕼOᑎ ᗯOᖇKՏ ՏᑌᑕᑕEՏՏᖴᑌᒪᒪY ‌‌‏𓅓 . **"
+    TEP_IMG = gvarstatus("ALIVE_PIC")
+    tep_caption = gvarstatus("ALIVE_TEMPLATE") or tep_temp
+    caption = tep_caption.format(
+        ALIVE_TEXT=ALIVE_TEXT,
+        T_EMOJI=T_EMOJI,
+        mention=mention,
+        uptime=uptime,
+        telever=version.__version__,
+        tepver=tepversion,
+        pyver=python_version(),
+        dbhealth=check_sgnirts,
+        ping=ms,
+        Tepthon_Tare5=installation_time
+    )
+    if TEP_IMG:
+        TEP = [x for x in TEP_IMG.split()]
+        PIC = random.choice(TEP)
+        try:
+            await event.client.send_file(
+                event.chat_id, PIC, caption=caption, reply_to=reply_to_id
+            )
+            await tepevent.delete()
+        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
+            return await edit_or_reply(
+                tepevent,
+                f"**⌔∮ عـذراً عليـك الـرد ع صـوره او ميـديـا  ⪼  `.اضف صورة الفحص` <بالرد ع الصـوره او الميـديـا> ",
+            )
+    else:
+        await edit_or_reply(
+            tepevent,
+            caption,
+        )
+
+
+tep_temp = """{ALIVE_TEXT}
+———————⛥ ———————
+**{T_EMOJI} َTEᒪETᕼOᑎ 𓋪** `{telever}`
+**{T_EMOJI} TEᑭTᕼOᑎ 𓋪** `{zdver}`
+**{T_EMOJI} َᑭYTᕼOᑎ 𓋪** `{pyver}`
+**{T_EMOJI} ᑌᑭTIᗰE 𓋪** `{uptime}`
+**{T_EMOJI} OᗯᑎEᖇ 𓋪** {mention}
+**{T_EMOJI} ᗪᗩTᗴ 𓋪** {Tepthon_Tare5}"""
+     send_new_message = await event.client.send_message(entity=event.chat_id, message=final_message, file=random.choice(RANDOM_MEDIA))
 
 
 @zedub.zed_cmd(
@@ -36,61 +103,21 @@ STATS = gvarstatus("Z_STATS") or "فحص"
         ],
     },
 )
-async def amireallyalive(event):
+async def amireallyialive(event):
+    "A kind of showing bot details by your inline bot"
     reply_to_id = await reply_id(event)
-    uptime = await get_readable_time((time.time() - StartTime))
-    start = datetime.now()
-    zedevent = await edit_or_reply(event, "**⛥ ⤻ انتـظࢪ جـاࢪي فـحص بـ ـوت TEᑭTᕼOᑎ الخـاص بـِك   ۦ**")
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    _, check_sgnirts = check_data_base_heal_th()
-    random_media = ["https://telegra.ph/file/6fa698c2e409a2bf41bd6.mp4","https://telegra.ph/file/1131ec8fee887ddcc06bd.mp4"]
-    RNRYRTM = time.strftime("%I:%M")
-    RNRYRDATE = time.strftime("%Y/%m/%d")
-    Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "•"
-    ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** سۅٛࢪس تيبثۅٛن يعمݪ بنجاެح **"
-    ZED_IMG = gvarstatus("ALIVE_PIC")
-    zed_caption = gvarstatus("ALIVE_TEMPLATE") or zed_temp
-    caption = zed_caption.format(
-        ALIVE_TEXT=ALIVE_TEXT,
-        Z_EMOJI=Z_EMOJI,
-        mention=mention,
-        uptime=uptime,
-        telever=version.__version__,
-        zdver=zedversion,
-        pyver=python_version(),
-        dbhealth=check_sgnirts,
-        ping=ms,
-        RNRYRTM=RNRYRTM,
-        RNRYRDATE=RNRYRDATE,
-    )
-    if ZED_IMG:
-        ZED = [x for x in ZED_IMG.split()]
-        PIC = random.choice(ZED)
-        try:
-            await event.client.send_file(
-                event.chat_id, PIC, caption=caption, reply_to=reply_to_id
-            )
-            await zedevent.delete()
-        except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
-            return await edit_or_reply(
-                zedevent,
-                f"**⌔∮ عـذراً عليـك الـرد ع صـوره او ميـديـا  ⪼  `.اضف صورة الفحص` <بالرد ع الصـوره او الميـديـا> ",
-            )
-    else:
-        await edit_or_reply(
-            zedevent,
-            caption,
-        )
+    T_EMOJI = gvarstatus("ALIVE_EMOJI") or "☼ ⤶"
+    tep_caption = "** ☼ TEᑭTᕼOᑎ ᗯOᖇKՏ ՏᑌᑕᑕEՏՏᖴᑌᒪᒪY ‌‌‏𓅓 . **\n"
+    tep_caption += f"**{T_EMOJI} َTEᒪETᕼOᑎ 𓋪** `{version.__version__}\n`"
+    tep_caption += f"**{T_EMOJI} TEᑭTᕼOᑎ 𓋪 :** `{zedversion}`\n"
+    tep_caption += f"**{T_EMOJI} َᑭYTᕼOᑎ 𓋪** `{python_version()}\n`"
+    tep_caption += f"**{T_EMOJI} OᗯᑎEᖇ 𓋪** {mention}\n"
+    results = await event.client.inline_query(Config.TG_BOT_USERNAME, zed_caption)
+    await results[0].click(event.chat_id, reply_to=reply_to_id, hide_via=True)
+    await event.delete()
 
 
-zed_temp = """{ALIVE_TEXT}
---  --  --  --  --  --  --  --
-{Z_EMOJI} اެصداެࢪ اެݪتيݪيثۅٛن : `{telever}` .
-{Z_EMOJI} اެصداެࢪ اެݪسۅٛࢪس : `{zdver}` .
-{Z_EMOJI} اެصداެࢪ اެݪباެيثۅٛن : `{pyver}` .
-{Z_EMOJI} اެݪتاެࢪيخ : `{RNRYRDATE}` .
-{Z_EMOJI} اެݪۅٛقت : `{RNRYRTM}` .
-{Z_EMOJI} ۅٛقت اެݪتشغيݪ : `{uptime}` .
-{Z_EMOJI} اެݪماެݪك : {mention} .\n--  --  --  --  --  --  --  --"""
-    send_new_message = await event.client.send_message(entity=event.chat_id, message=final_message, file=random.choice(random_media))
+@zedub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
+async def on_plug_in_callback_query_handler(event):
+    statstext = await zedalive(StartTime)
+    await event.answer(statstext, cache_time=0, alert=True)
