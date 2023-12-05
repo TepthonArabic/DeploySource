2610c4da93660121f47a583e556733e6ef426198
روجر <104784913+rogerpq@users.noreply.github.com>
2023-12-05 20:24:52 +0300

    Update __main__.py

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -4,11 +4,10 @@ from Tepthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, saves
+from .utils import mybot, autoname, autovars
 from .utils import (
     add_bot_to_logger_group,
     load_plugins,
-    saves,
     setup_bot,
     startupmessage,
     verifyLoggerGroup,
@@ -22,17 +21,25 @@ print(f"المرخصة بموجب شروط  {Tepthon.__license__}")
 cmdhr = Config.COMMAND_HAND_LER
 
 try:
-    LOGS.info("⌭ بـدء تنزيـل تيـبثون ⌭")
-    zedub.loop.run_until_complete(setup_bot())
-    LOGS.info("⌭ بـدء تشغيـل البـوت ⌭")
+    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
+    zedub.loop.run_until_complete(autovars())
+    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
 except Exception as e:
     LOGS.error(f"{e}")
     sys.exit()
 
+if not Config.ALIVE_NAME:
+    try: #Code by T.me/E_7_V
+        LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
+        zedub.loop.run_until_complete(autoname())
+        LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
+    except Exception as e:
+        LOGS.error(f"- {e}")
+
 try: #Code by T.me/E_7_V
-    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
-    zedub.loop.run_until_complete(saves())
-    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
+    LOGS.info("⌭ بـدء تنزيـل تيـبثون ⌭")
+    zedub.loop.run_until_complete(setup_bot())
+    LOGS.info("⌭ بـدء تشغيل البـوت ⌭")
 except Exception as e:
     LOGS.error(f"- {e}")
 
@@ -52,7 +59,7 @@ async def startup_process():
     print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
     print("تـم التنصـيب .. بنجـاح ✓")
     print(
-        f"⌔┊تـم تنصيـب تيـبثون يـوزربـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  حمد\n⌔┊قنـاة السـورس ↶.\n🌐┊@Tepthon"
+        f"⌔┊تـم تنصيـب تيبثـون يـوزر بـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  محـمد\n⌔┊قنـاة السـورس ↶.\n🌐┊@Tepthon"
     )
     print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
     await verifyLoggerGroup()
