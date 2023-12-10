8e72dbaef07b7196760333b037c2396314f1de98
Hussein <118964436+SS7SS@users.noreply.github.com>
2023-12-10 16:24:19 +0300

    Create فحص معرف.py

---
+++ Tepthon/plugins/فحص معرف.py
@@ -0,0 +1,16 @@
+from Tepthon import zedub
+from Tepthon.core.logger import logging
+import re
+import requests
+from telethon.sync import TelegramClient, events
+
+@zedub.zed_cmd.on(events.NewMessage(pattern='/nft'))
+async def checknft(event):
+    try:
+        chat_id = event.chat_id
+        nft = event.text.split()[1]
+        img = f'https://nft.fragment.com/username/{nft}.webp'
+        message = await client.send_file(chat_id, img, caption=f'*Yes, this is the nft : {nft}*', parse_mode="Markdown")
+        await client.send_message(chat_id, f'Yes, this is the nft : {nft}', reply_to=message)
+    except:
+        await event.reply(f'*Sorry, this username is not nft : {nft}*', parse_mode="Markdown")
