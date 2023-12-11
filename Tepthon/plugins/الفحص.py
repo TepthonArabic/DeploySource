75f2f8d26e937db92396a1aba156fa7280b389cb
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2023-12-11 17:27:35 +0300

    update الفحص.py

--- Tepthon/plugins/الفحص.py
+++ Tepthon/plugins/الفحص.py
@@ -14,11 +14,11 @@ from telethon.errors.rpcerrorlist import (
 )
 from telethon.events import CallbackQuery
 
-from . import StartTime, zedub, tepversion
+from . import StartTime, zedub, zedversion
 
 from ..Config import Config
 from ..core.managers import edit_or_reply
-from ..helpers.functions import tepalive, check_data_base_heal_th, get_readable_time
+from ..helpers.functions import zedalive, check_data_base_heal_th, get_readable_time
 from ..helpers.utils import reply_id
 from ..sql_helper.globals import gvarstatus
 from . import mention
@@ -34,7 +34,7 @@ async def amireallyalive(event):
     boot_time_timestamp = psutil.boot_time()
     bt = datetime.fromtimestamp(boot_time_timestamp)
     start = datetime.now()
-    tepevent = await edit_or_reply(event, "**✦ | جـارِ فحـص سـورس ᴛᴇᴘᴛʜᴏɴ العربي ...**")
+    zedevent = await edit_or_reply(event, "**✦ | جـارِ فحـص سـورس ᴛᴇᴘᴛʜᴏɴ العربي ...**")
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     _, check_sgnirts = check_data_base_heal_th()
@@ -47,7 +47,7 @@ async def amireallyalive(event):
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✦┊"
     ALIVE_TEXT = gvarstatus("ALIVE_TEXT") or "** ᴀʀᴀʙ ᴛᴇᴘᴛʜᴏɴ ѕᴏᴜʀᴄᴇ ɪѕ ᴡᴏʀᴋɪɴɢ ѕᴜᴄᴄᴇѕѕғᴜʟʟʏ - سـورس تيبثـون العربـي يعمـلُ بنجـاح **"
     ZED_IMG = gvarstatus("ALIVE_PIC")
-    zed_caption = gvarstatus("ALIVE_TEMPLATE") or tep_temp
+    zed_caption = gvarstatus("ALIVE_TEMPLATE") or zed_temp
     caption = zed_caption.format(
         ALIVE_TEXT=ALIVE_TEXT,
         Z_EMOJI=Z_EMOJI,
@@ -55,7 +55,7 @@ async def amireallyalive(event):
         uptime=uptime,
         zedda=zedda,
         telever=version.__version__,
-        zdver=tepversion,
+        zdver=zedversion,
         pyver=python_version(),
         dbhealth=check_sgnirts,
         ping=ms,
@@ -67,20 +67,20 @@ async def amireallyalive(event):
             await event.client.send_file(
                 event.chat_id, PIC, caption=caption, reply_to=reply_to_id
             )
-            await tepevent.delete()
+            await zedevent.delete()
         except (WebpageMediaEmptyError, MediaEmptyError, WebpageCurlFailedError):
             return await edit_or_reply(
-                tepevent,
+                zedevent,
                 f"**⌔∮ عـذراً عليـك الـرد ع صـوره او ميـديـا  ⪼  `.اضف صورة الفحص` <بالرد ع الصـوره او الميـديـا> ",
             )
     else:
         await edit_or_reply(
-            tepevent,
+            zedevent,
             caption,
         )
 
 
-tep_temp = """{ALIVE_TEXT}
+zed_temp = """{ALIVE_TEXT}
 
 **{Z_EMOJI} قاعدة البيانات :** تعمـل بـ نجـاح ♾
 **{Z_EMOJI} إصـدار المكتبـة :** `{telever}`
@@ -89,7 +89,7 @@ tep_temp = """{ALIVE_TEXT}
 **{Z_EMOJI} وقت التشغيل :** `{uptime}`
 **{Z_EMOJI} تاريـخ التنصيب :** `{zedda}`
 **{Z_EMOJI} المسـتخـدم:** {mention}
-**{Z_EMOJI} قنـاة السـورس :** [اضغـط هنـا](https://t.me/Tepthon)"""
+**{Z_EMOJI} قنـاة السـورس :** [اضغـط هنـا](https://t.me/zedthon)"""
 
 
 @zedub.zed_cmd(
@@ -108,7 +108,7 @@ async def amireallyialive(event):
     Z_EMOJI = gvarstatus("ALIVE_EMOJI") or "✦┊"
     zed_caption = "** ᴀʀᴀʙ ᴛᴇᴘᴛʜᴏɴ ѕᴏᴜʀᴄᴇ ɪѕ ᴡᴏʀᴋɪɴɢ ѕᴜᴄᴄᴇѕѕғᴜʟʟʏ - سـورس تيبثـون العربـي يعمـلُ بنجـاح **\n"
     zed_caption += f"**{Z_EMOJI} إصـدار التـيليثون :** `{version.__version__}\n`"
-    zed_caption += f"**{Z_EMOJI} إصـدار تيبثـون :** `{tepversion}`\n"
+    zed_caption += f"**{Z_EMOJI} إصـدار تيبثـون :** `{zedversion}`\n"
     zed_caption += f"**{Z_EMOJI} إصـدار البـايثون :** `{python_version()}\n`"
     zed_caption += f"**{Z_EMOJI} المسـتخدم :** {mention}\n"
     results = await event.client.inline_query(Config.TG_BOT_USERNAME, zed_caption)
@@ -118,6 +118,6 @@ async def amireallyialive(event):
 
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"stats")))
 async def on_plug_in_callback_query_handler(event):
-    statstext = await tepalive(StartTime)
+    statstext = await zedalive(StartTime)
   #كتابـة_زلـزال_الهيبـة
     await event.answer(statstext, cache_time=0, alert=True)
