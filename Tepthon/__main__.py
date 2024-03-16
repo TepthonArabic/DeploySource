3c76b0d4bf92dd4b75917a2a001ab3eecc53c60e
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-03-16 15:59:12 +0300

    Update __main__.py

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -1,81 +1,85 @@
-import sys
+import sys, asyncio
 import Tepthon
-from Tepthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+from Tepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
+from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, autoname, autovars
-from .utils import (
-    add_bot_to_logger_group,
-    load_plugins,
-    setup_bot,
-    startupmessage,
-    verifyLoggerGroup,
-)
+from .utils import mybot, autoname, autovars, saves
+from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
 
-LOGS = logging.getLogger("سورس تيبثون")
+LOGS = logging.getLogger("سـورس تيبثـون")
+cmdhr = Config.COMMAND_HAND_LER
 
 print(Tepthon.__copyright__)
 print(f"المرخصة بموجب شروط  {Tepthon.__license__}")
 
 cmdhr = Config.COMMAND_HAND_LER
 
-try:
-    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
+try: #Code by T.me/zzzzl1l
+    LOGS.info("⌭ جـارِ تحميـل الملحقـات ⌭")
     zedub.loop.run_until_complete(autovars())
     LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
 except Exception as e:
-    LOGS.error(f"{e}")
-    sys.exit()
+    LOGS.error(f"- {e}")
 
 if not Config.ALIVE_NAME:
-    try: #Code by T.me/E_7_V
+    try: #Code by T.me/zzzzl1l
         LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
         zedub.loop.run_until_complete(autoname())
         LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
     except Exception as e:
         LOGS.error(f"- {e}")
 
-try: #Code by T.me/E_7_V
-    LOGS.info("⌭ بـدء تنزيـل تيـبثون ⌭")
+try:
+    LOGS.info("⌭ بـدء تنزيـل تيبثــون ⌭")
     zedub.loop.run_until_complete(setup_bot())
-    LOGS.info("⌭ بـدء تشغيل البـوت ⌭")
+    LOGS.info("✓ تـم تنزيـل تيبثــون .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"{str(e)}")
+    sys.exit()
+
+class CatCheck:
+    def __init__(self):
+        self.sucess = True
+Catcheck = CatCheck()
+
+try:
+    LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("✓ تـم إنشـاء البـوت .. بـنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
 
 try:
-    LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
-    zedub.loop.run_until_complete(mybot())
-    LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
+    LOGS.info("⌭ جـارِ تفعيـل الاشتـراك ⌭")
+    zedub.loop.create_task(saves())
+    LOGS.info("✓ تـم تفعيـل الاشتـراك .. بنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
-
 
 
 async def startup_process():
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-async def qruan_process():
-    await verifyLoggerGroup()
-    await load_plugins("vc_baqir")
-    print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
-    print("تـم التنصـيب .. بنجـاح ✓")
-    print(
-        f"⌔┊تـم تنصيـب تيبثـون يـوزر بـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي ..  محـمد\n⌔┊قنـاة السـورس ↶.\n🌐┊@Tepthon"
-    )
-    print("➖➖➖➖➖ سورس تيبثون™ ➖➖➖➖➖")
+    LOGS.info(f"⌔ تـم تنصيـب تيبثــون . . بنجـاح ✓ \n⌔ لـ إظهـار الاوامـر أرسـل (.الاوامر)")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
+    Catcheck.sucess = True
     return
 
+
 zedub.loop.run_until_complete(startup_process())
-zedub.loop.run_until_complete(qruan_process())
+
 if len(sys.argv) not in (1, 3, 4):
     zedub.disconnect()
+elif not Catcheck.sucess:
+    if HEROKU_APP is not None:
+        HEROKU_APP.restart()
 else:
     try:
         zedub.run_until_disconnected()
