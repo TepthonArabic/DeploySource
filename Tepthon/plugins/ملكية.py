77f75d5cef4bc1d4ee4a972b39e45945d11b9f9c
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-05-17 13:09:08 +0300

    Update ملكية.py

--- Tepthon/plugins/ملكية.py
+++ Tepthon/plugins/ملكية.py
@@ -18,14 +18,14 @@ plugin_category = "الادوات"
     command=("تحويل ملكية", plugin_category),
     info={
         "header": "لـ تحويـل ملكيـة القنـاة او الكـروب",
-        "الاستخـدام": "{tr}تحويل ملكية + معـرف الشخص الذي تريد نقل الملكيـه اليـه",
+        "الاستخـدام": "{tr}تحويل ملكية + معـرف الشخص الذي تريد نقل الملكيـه إليه",
     },
 )
 async def _(event):
     "لتحويل مُلكية قنـاة أو مجموعـة"
     user_name = event.pattern_match.group(1)
     if gvarstatus("TG_2STEP_VERIFICATION_CODE") is None:
-        return await edit_or_reply(event, "**⎉╎قم أولًا بـ إضافـة كـود التحقق بخطوتين الخـاص بك لـ الفـارات **\n**⎉╎عبـر الأمر : ↶**\n `.اضف التحقق` **بالـرد علـى كـود التحقق الخـاص بك**\n\n**⎉╎ثم أرسل الأمر : ↶**\n`.تحويل ملكية` **ومعـرف الشخص**\n\n**⎉╎لتحويـل ملكيـة القنـاة/المجموعة للشخـص**")
+        return await edit_or_reply(event, "**⎉╎قم أولًا بـ إضافـة كـود التحقق بخطوتين الخـاص بك لـ الفـارات **\n**⎉╎عبـر الأمر : ↶**\n `.اضف فار التحقق` **بالـرد علـى كـود التحقق الخـاص بك**\n\n**⎉╎ثم أرسل الأمر : ↶**\n`.تحويل ملكية` **ومعـرف الشخص**\n\n**⎉╎لتحويـل ملكيـة القنـاة/المجموعة للشخـص**")
     try:
         pwd = await event.client(functions.account.GetPasswordRequest())
         my_srp_password = pwd_mod.compute_check(pwd, gvarstatus("TG_2STEP_VERIFICATION_CODE"))
