c71337f4632356d7341c652d4bae4102d05f7530
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-12 12:32:54 +0300

    Update النقاط.py

--- Tepthon/plugins/النقاط.py
+++ Tepthon/plugins/النقاط.py
@@ -32,6 +32,8 @@ ZelzalCoins_cmd = (
     "**⪼ لـ تجميـع النقـاط مـن بـوت الجوكـر ( @A_MAN9300BOT ) .. تلقائيًّـا ✓**\n\n"
     "`.الجنرال`\n"
     "**⪼ لـ تجميـع النقـاط مـن بـوت العقــاب ( @MARKTEBOT ) .. تلقائيًّـا ✓**\n\n"
+      "`.آسياسيل`\n"
+    "**⪼ لـ تجميـع النقـاط مـن بـوت آسياسيـل ( @MARKTEBOT ) .. تلقائيًّـا ✓**\n\n"
     "`.المليون`\n"
     "**⪼ لـ تجميـع النقـاط مـن بـوت المليــون ( @qweqwe1919bot ) .. تلقائيًّـا ✓**\n\n\n"
     "`.سمسم`\n"
@@ -317,6 +319,53 @@ async def _(event):
     await zedub.send_message(event.chat_id, "**⎉╎تم الانتهـاء مـن تجميـع النقـاط .. حاول من جديد في وقت آخر ✓**")
 
 
+@zedub.zed_cmd(pattern="بوت آسياسيل$")
+async def _(event):
+    await event.edit('@yynnurybot')
+
+# Copyright (C) 2022 Zed-Thon . All Rights Reserved
+@zedub.zed_cmd(pattern="(آسياسيل|تجميع آسياسيل)(?: |$)(.*)")
+async def _(event):
+    con = event.pattern_match.group(1).lower()
+    await event.edit("**⎉╎حسنًـا .. تأكـد من أنـك مشتـرك بـ قنـوات الاشتـراك الإجبـاري لتجنب الأخطـاء @yynnurybot**")
+    channel_entity = await zedub.get_entity('@yynnurybot')
+    await zedub.send_message('@yynnurybot', '/start')
+    await asyncio.sleep(4)
+    msg0 = await zedub.get_messages('@yynnurybot', limit=1)
+    await msg0[0].click(2)
+    await asyncio.sleep(4)
+    msg1 = await zedub.get_messages('@yynnurybot', limit=1)
+    await msg1[0].click(0)
+    chs = 1
+    for i in range(100):
+        await asyncio.sleep(10)
+        list = await zedub(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0, add_offset=0, hash=0))
+        msgs = list.messages[0]
+        if msgs.message.find('**⎉╎لا يوجد قنوات في الوقت الحالي .. قم يتجميع النقاط بطريقة مختلفة**') != -1:
+            await zedub.send_message(event.chat_id, "**⎉╎مـافي قنـوات بالبـوت حاليًّـا ...**")
+            break
+        if con == "ايقاف": #Code by T.me/zzzzl1l
+            await zedub.send_message(event.chat_id, "**⎉╎تم إيقـاف تجميـع النقـاط .. بنجـاح☑️**")
+            break
+        url = msgs.reply_markup.rows[0].buttons[0].url
+        try:
+            try:
+                await zedub(JoinChannelRequest(url))
+            except:
+                bott = url.split('/')[-1]
+                await zedub(ImportChatInviteRequest(bott))
+            msg2 = await zedub.get_messages('@yynnurybot', limit=1)
+            await msg2[0].click(text='تحقق')
+            chs += 1
+            await event.edit(f"**⎉╎تم الاشتـراك في القنـاة  {chs} ...✓**")
+        except: #Code by T.me/zzzzl1l
+            msg2 = await zedub.get_messages('@yynnurybot', limit=1)
+            await msg2[0].click(text='التالي')
+            chs += 1
+            await event.edit(f"**⎉╎القنـاة رقـم {chs} .. يمكـن تبنـدت**")
+    await zedub.send_message(event.chat_id, "**⎉╎تم الانتهـاء مـن تجميـع النقـاط .. حاول من جديد في وقت آخر ✓**")
+    
+    
 @zedub.zed_cmd(pattern="بوت العقاب$")
 async def _(event):
     await event.edit('@MARKTEBOT')
