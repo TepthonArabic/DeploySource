8c50755f42febe2894c907e1f7b7c3cafbbd62ff
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-15 23:52:13 +0300

    Update server.py

--- Tepthon/core/server.py
+++ Tepthon/core/server.py
@@ -10,4 +10,4 @@ class Greeting (Resource):
         return "Tepthon is Up & Running!"
 
 api.add_resource(Greeting, '/')
-app.run(host="0.0.0.0", port=os.environ.get("PORT", 4000))
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
