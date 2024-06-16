0ae4346dbf77f2d16a2a1e1d8e8baf501963b73b
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-16 09:12:12 +0300

    Update __main__.py

--- Tepthon/__main__.py
+++ Tepthon/__main__.py
@@ -1,4 +1,3 @@
-from telethon.tl.functions.messages import GetMessagesViewsRequest
 import sys, asyncio
 import Tepthon
 from Tepthon import BOTLOG_CHATID, HEROKU_APP, PM_LOGGER_GROUP_ID
@@ -6,91 +5,60 @@ from telethon import functions
 from .Config import Config
 from .core.logger import logging
 from .core.session import zedub
-from .utils import mybot, saves
+from .utils import mybot, saves, autoname
 from .utils import add_bot_to_logger_group, load_plugins, setup_bot, startupmessage, verifyLoggerGroup
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
-LOGS = logging.getLogger("سورس تيبثون")
+LOGS = logging.getLogger("Tepthon")
 cmdhr = Config.COMMAND_HAND_LER
 
-print(Tepthon.__copyright__)
-print(f"المرخصة بموجب شروط  {Tepthon.__license__}")
-
-cmdhr = Config.COMMAND_HAND_LER
+if gvarstatus("ALIVE_NAME") is None: #Code by T.me/zzzzl1l
+    try:
+        LOGS.info("⌭ بـدء إضافة الاسـم التلقـائـي ⌭")
+        zedub.loop.run_until_complete(autoname())
+        LOGS.info("✓ تـم إضافة فار الاسـم .. بـنجـاح ✓")
+    except Exception as e:
+        LOGS.error(f"- The AutoName {e}")
 
 try:
-    LOGS.info("✧ بـدء تنزيـل تيبــثون ✧")
+    LOGS.info("⌭ بـدء تنزيـل تيبثـون ⌭")
     zedub.loop.run_until_complete(setup_bot())
-    LOGS.info("✧ بـدء تشغيـل البـوت ✧")
+    LOGS.info("✓ تـم تنزيـل تيبثـون .. بـنجـاح ✓")
 except Exception as e:
     LOGS.error(f"{str(e)}")
     sys.exit()
 
-class CatCheck:
+class ZTCheck:
     def __init__(self):
         self.sucess = True
-Catcheck = CatCheck()
+ZTcheck = ZTCheck()
 
 try:
-    LOGS.info("✧ جـار تفعيـل وضـع الانـلاين ✧")
+    LOGS.info("⌭ بـدء إنشـاء البـوت التلقـائـي ⌭")
     zedub.loop.run_until_complete(mybot())
-    LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
+    LOGS.info("✓ تـم إنشـاء البـوت .. بـنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
 
-
 try:
-    LOGS.info("✧ جـاري تحميـل الملحقـات ✧")
+    LOGS.info("⌭ جـارِ تفعيـل الاشتـراك ⌭")
     zedub.loop.create_task(saves())
-    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
+    LOGS.info("✓ تـم تفعيـل الاشتـراك .. بنجـاح ✓")
 except Exception as e:
     LOGS.error(f"- {e}")
 
 
 async def startup_process():
-    async def MarkAsViewed(channel_id):
-        from telethon.tl.functions.channels import ReadMessageContentsRequest
-        try:
-            channel = await zedub.get_entity(channel_id)
-            async for message in zedub.iter_messages(entity=channel.id, limit=5):
-                try:
-                    await zedub(GetMessagesViewsRequest(peer=channel.id, id=[message.id], increment=True))
-                except Exception as error:
-                    print ("✅")
-            return True
-
-        except Exception as error:
-            print ("✅")
-
-    async def start_bot():
-      try:
-          List = ["Tepthon","Tepthone1","Tws_Tepthon","Tepthon_Support","VVV5P","tepthonklaesh"]
-          from telethon.tl.functions.channels import JoinChannelRequest
-          for id in List :
-              Join = await zedub(JoinChannelRequest(channel=id))
-              MarkAsRead = await MarkAsViewed(id)
-              print (MarkAsRead, "✅")
-          return True
-      except Exception as e:
-        print("✅")
-        return False
-    
     await verifyLoggerGroup()
     await load_plugins("plugins")
     await load_plugins("assistant")
-    print(f"⌔┊تـم تنصيـب تيبــثون . . بنجـاح ✓ \n⌔┊لـ إظهـار الاوامـر ارسـل (.الاوامر)")
+    LOGS.info(f"⌔ تـم تنصيـب تيبثـون . . بنجـاح ✓ \n⌔ لـ إظهـار الأوامـر أرسـل (.الاوامر)")
     await verifyLoggerGroup()
     await add_bot_to_logger_group(BOTLOG_CHATID)
     if PM_LOGGER_GROUP_ID != -100:
         await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
     await startupmessage()
-    Catcheck.sucess = True
-    
-    Checker = await start_bot()
-    if Checker == False:
-        print("#1")
-    else:
-        print ("✅")
-    
+    ZTcheck.sucess = True
     return
 
 
@@ -98,9 +66,11 @@ zedub.loop.run_until_complete(startup_process())
 
 if len(sys.argv) not in (1, 3, 4):
     zedub.disconnect()
-elif not Catcheck.sucess:
-    if HEROKU_APP is not None:
-        HEROKU_APP.restart()
+elif not ZTcheck.sucess:
+    try:
+        zedub.run_until_disconnected()
+    except ConnectionError:
+        pass
 else:
     try:
         zedub.run_until_disconnected()
