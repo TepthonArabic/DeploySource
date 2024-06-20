e55d009508f2e5d4865b3709b0e6c9c88077e06d
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-20 13:15:34 +0300

    Update server.py

--- Tepthon/core/server.py
+++ Tepthon/core/server.py
@@ -1,9 +1,13 @@
-from aiohttp import web
+import os
+from flask import Flask
+from flask_restful import Resource, Api
 
-from .route import routes
+app = Flask(__name__)
+api = Api(app)
 
+class Greeting (Resource):
+    def get(self):
+        return "Tepthon is Up & Running!"
 
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
+api.add_resource(Greeting, '/')
+app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
