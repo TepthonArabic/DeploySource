01684c1efb96c515925d5e9d3f486381c5b64c51
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-24 09:38:08 +0300

    Create could.sh

---
+++ could.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+cat <<'EOF'
+$$$$$$$$\ $$$$$$$$\ $$\                           
+\____$$  |\__$$  __|$$ |                          
+    $$  /    $$ |   $$$$$$$\   $$$$$$\  $$$$$$$\  
+   $$  /     $$ |   $$  __$$\ $$  __$$\ $$  __$$\ 
+  $$  /      $$ |   $$ |  $$ |$$ /  $$ |$$ |  $$ |
+ $$  /       $$ |   $$ |  $$ |$$ |  $$ |$$ |  $$ |
+$$$$$$$$\    $$ |   $$ |  $$ |\$$$$$$  |$$ |  $$ |
+\________|   \__|   \__|  \__| \______/ \__|  \__|
+                                                  
+                                                  
+                                                  
+Copyright (C) 2020-2024 by Zelzal-SC@Github, < https://github.com/RRYR7 >.
+This file is part of < https://github.com/RRYR7/Koyeb > project,
+and is released under the "GNU v3.0 License Agreement".
+Please see < https://github.com/RRYR7/koyeb/blob/main/LICENSE >
+All rights reserved.
+EOF
+
+gunicorn app:app --daemon && python -m Tepthon
