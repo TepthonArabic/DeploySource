2c33bf09668ec3021010522d7ccfc34adde19706
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-06-24 10:13:51 +0300

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -12,7 +12,7 @@ $$$$$$$$\    $$ |   $$ |  $$ |\$$$$$$  |$$ |  $$ |
                                                   
                                                   
                                                   
-Copyright (C) 2020-2024 by Zelzal-SC@Github, < https://github.com/RRYR7 >.
+Copyright (C) 2020-2024 by RRYR7@Github, < https://github.com/RRYR7 >.
 This file is part of < https://github.com/RRYR7/Koyeb > project,
 and is released under the "GNU v3.0 License Agreement".
 Please see < https://github.com/RRYR7/koyeb/blob/main/LICENSE >
