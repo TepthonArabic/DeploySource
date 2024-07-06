713a7e20af15acfff2cf1db4cd0da6759b1c119d
Mohammad Taleb <98346135+RRYR7@users.noreply.github.com>
2024-07-06 22:14:42 +0300

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -7,10 +7,10 @@ Installing Tepthon is in progress..
 
 
                                                   
-Copyright (C) 2020-2024 by RRYR7@Github, < https://github.com/RRYR7 >.
-This file is part of < https://github.com/RRYR7/Koyeb > project,
+Copyright (C) 2020-2024 by TepthonArabic@Github, < https://github.com/TepthonArabic >.
+This file is part of < https://github.com/TepthonArabic/DeploySource > project,
 and is released under the "GNU v3.0 License Agreement".
-Please see < https://github.com/RRYR7/koyeb/blob/main/LICENSE >
+Please see < https://github.com/TepthonArabic/DeploySource/blob/main/LICENSE >
 All rights reserved.
 EOF
 
