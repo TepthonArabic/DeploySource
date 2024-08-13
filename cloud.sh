8ec182a836a1a6ab5fde1242b5d08d04559b48e5
Mohammad Tepthon <98346135+zxaax@users.noreply.github.com>
2024-08-14 01:19:12 +0300

    Update cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -8,9 +8,9 @@ Installing Tepthon is in progress..
 
                                                   
 Copyright (C) 2020-2024 by TepthonArabic@Github, < https://github.com/TepthonArabic >.
-This file is part of < https://github.com/TepthonArabic/DeploySource > project,
+This file is part of < https://github.com/TepthonArabic/DeploySo > project,
 and is released under the "GNU v3.0 License Agreement".
-Please see < https://github.com/TepthonArabic/DeploySource/blob/main/LICENSE >
+Please see < https://github.com/TepthonArabic/DeploySo/blob/main/LICENSE >
 All rights reserved.
 EOF
 
